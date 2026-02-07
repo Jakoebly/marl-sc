64d1b57609a1cf2312d57cd566b5da8a6957196d
Jakob Ehrenhuber <jakoebly@gmail.com>
2026-02-07 08:05:43 -0500

    Updated run_experiment_array.sh

--- scripts/run_experiment_array.sh
+++ scripts/run_experiment_array.sh
@@ -14,7 +14,7 @@
 #SBATCH --chdir=/home/jakobeh/projects/marl-sc  # Working directory
 #SBATCH --output=scripts/logs/%x_%A_%a.out      # Standard output
 #SBATCH --error=scripts/logs/%x_%A_%a.err       # Standard error
-#SBATCH --array=0-48%1                          # Array for 49 jobs (indices 0-48) with 5 jobs at once per node
+#SBATCH --array=0-24%1                          # Array for 25 jobs (indices 0-24) with 1 job at once per node
 
 
 ##############################
@@ -40,7 +40,7 @@ SKU_VALUES=(3 5 10 20 50 100)
 # Compute the number of possible tasks
 N_WHVALS=${#WAREHOUSE_VALUES[@]}   # 7
 N_SKUVALS=${#SKU_VALUES[@]}         # 7
-N_TASKS=$((N_WHVALS * N_SKUVALS))     # 49
+N_TASKS=$((N_WHVALS * N_SKUVALS))     # 24
 
 # Get the warehouse and SKU values for this task
 ID=${SLURM_ARRAY_TASK_ID}
