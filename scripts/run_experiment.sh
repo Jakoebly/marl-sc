11ed4ac7f76c91fad180e09abf9d2146db3ea481
Jakob Ehrenhuber <jakoebly@gmail.com>
2026-03-04 10:40:19 -0500

    Updated run_experiment.sh

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -145,6 +145,6 @@ python src/experiments/run_experiment.py \
     --env-config ./config_files/environments/env_custom_FixedLambda.yaml \
     --algorithm-config ./config_files/algorithms/ippo.yaml \
     --output-dir ./experiment_outputs \
-    --experiment-name "MAPPO_single_3WH_3SKUS_holding_cost_0.1_FixedSigma_LT1_FixedLambda_FixedParameters" \
+    --experiment-name "IPPO_single_3WH_3SKUS_holding_cost_0.1_FixedSigma_LT1_FixedLambda_FixedParameters" \
     --wandb-project marl-sc \
     --root-seed 42
