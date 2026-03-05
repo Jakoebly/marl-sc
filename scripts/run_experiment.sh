c2eba0695071900430891ab213170d9a198c4b4e
Jakob Ehrenhuber <jakoebly@gmail.com>
2026-03-05 16:43:23 -0500

    Updated run_experiment.sh

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -140,7 +140,7 @@ ray start --head \
 # Run training
 ##############################
 
-EXPERIMENT_NAME="IPPO_Single_3WH_3SKUS_Team_PSTrue_HC0.1_LowerPC"
+EXPERIMENT_NAME="IPPO_Single_3WH_3SKUS_Team_PSTrue_HC0.1_RatioNorm"
 
 python src/experiments/run_experiment.py \
     --mode single \
