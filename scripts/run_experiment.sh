c846ed73aeb5de8cfd713eddff44e45ad9fe348e
Jakob Ehrenhuber <jakoebly@gmail.com>
2026-03-05 10:32:46 -0500

    Updated run_experiment.sh

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -140,7 +140,7 @@ ray start --head \
 # Run training
 ##############################
 
-EXPERIMENT_NAME="IPPO_Single_3WH_3SKUS_Team_PSTrue_HC0.1_ObsNorm_TEST"
+EXPERIMENT_NAME="IPPO_Single_3WH_3SKUS_Team_PSTrue_HC0.1_NoObsNorm_TEST"
 
 python src/experiments/run_experiment.py \
     --mode single \
