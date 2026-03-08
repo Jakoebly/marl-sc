1fd32b1e580b96c47e585f94593356a91df78b86
Jakob Ehrenhuber <jakoebly@gmail.com>
2026-03-07 20:26:59 -0500

    Updated run_experiments.sh.

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -140,7 +140,7 @@ ray start --head \
 # Run training
 ##############################
 
-EXPERIMENT_NAME="IPPO_Single_3WH_2SKUS_SIMPLIFIED_Agent_PSFalse_MAXQTY40_NORMOFF"
+EXPERIMENT_NAME="IPPO_Single_3WH_2SKUS_SIMPLIFIED_Agent_PSFalse_MAXQTY30_NORMOFF"
 
 python src/experiments/run_experiment.py \
     --mode single \
