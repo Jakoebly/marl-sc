7fea9bd674633ab0748f24a686728cead262012e
Jakob Ehrenhuber <jakoebly@gmail.com>
2026-03-10 18:07:21 -0400

    Updated run_experiment.sh.

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -142,7 +142,7 @@ ray start --head \
 
 # Set output directory and experiment name
 OUTPUT_DIR="./experiment_outputs/WorkingConfig_Phase1.2.2"
-EXPERIMENT_NAME="IPPO_Single_3WH_2SKUS_Agent_PSFalse_MaxQty30_Scale1e-2"
+EXPERIMENT_NAME="IPPO_Single_3WH_2SKUS_Agent_PSFalse_MaxQty30_Scale1e-2_OBSMeanStCustom"
 
 python src/experiments/run_experiment.py \
     --mode single \
