c8289e2779680131f002968088ee6c98b90853fc
Jakob Ehrenhuber <jakoebly@gmail.com>
2026-03-13 08:40:19 -0400

    Updated run_baselines.sh.

--- scripts/run_baselines.sh
+++ scripts/run_baselines.sh
@@ -30,11 +30,11 @@ export PYTHONUNBUFFERED=1
 # Run baselines (no Ray needed)
 ##############################
 
-EXPERIMENT_NAME="BASELINES_SingleAgent_1WH_1SKU_Scale1e0"
+EXPERIMENT_NAME="BASELINES_Single_3WH_2SKUS_InitInv30"
 
 python src/experiments/run_baselines.py \
-    --env-config ./config_files/environments/env_simplified_single.yaml \
-    --output-dir ./experiment_outputs/WorkingConfig_Phase1.2.2 \
+    --env-config ./config_files/environments/env_simplified_symmetric.yaml \
+    --output-dir ./experiment_outputs/WorkingConfig_Phase1.2 \
     --experiment-name "${EXPERIMENT_NAME}" \
     --num-episodes 5 \
     --root-seed 42
