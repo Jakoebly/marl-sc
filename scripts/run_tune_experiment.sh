173272286ee744b6c882cf953c259cd3490d8374
Jakob Ehrenhuber <jakoebly@gmail.com>
2026-03-22 18:49:06 -0400

    Updated run_tune_experiment.sh

--- scripts/run_tune_experiment.sh
+++ scripts/run_tune_experiment.sh
@@ -149,7 +149,7 @@ ray start --head \
 
 # Set output directory and experiment name
 STORAGE_DIR="/home/jakobeh/projects/marl-sc/experiment_outputs/Tuning"
-EXPERIMENT_NAME="IPPO_Tune_3WH_2SKUS_Optuna_ASHA_SimplifiedEnv_V3"
+EXPERIMENT_NAME="IPPO_Tune_3WH_2SKUS_Optuna_ASHA_SimplifiedEnv"
 
 python src/experiments/run_experiment.py \
     --mode tune \
